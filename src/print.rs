9cc43aaa42ffbe923334f053d89c4085721e5c77
Jay T <jaytula@gmail.com>
2021-02-25 21:10:50 -0800

    Named Arguments example. In 16:45

--- src/print.rs
+++ src/print.rs
@@ -9,5 +9,8 @@ pub fn run() {
   println!("{} is from {}", "Brad", "Mass");
 
   // Positional Arguments
-  println!("{0} is from {1} and {0} like to {2}", "Brad", "Mass", "code")
+  println!("{0} is from {1} and {0} like to {2}", "Brad", "Mass", "code");
+
+  // Named Arguments
+  println!("{name} likes to play {activity}", name = "John", activity = "Baseball" );
 }
