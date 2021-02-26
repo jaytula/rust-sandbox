0057105c2cbe38cdb9039b1f2f9d692b3118c972
Jay T <jaytula@gmail.com>
2021-02-25 21:08:45 -0800

    Positional Arguments example. In 15:43

--- src/print.rs
+++ src/print.rs
@@ -7,4 +7,7 @@ pub fn run() {
 
   // Basic Formatting. Multiple placeholders
   println!("{} is from {}", "Brad", "Mass");
+
+  // Positional Arguments
+  println!("{0} is from {1} and {0} like to {2}", "Brad", "Mass", "code")
 }
