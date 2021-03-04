a4d9d2b7b4ec964d0237453c6c079f17ad568ac7
Jay T <jaytula@gmail.com>
2021-03-04 00:04:51 -0800

    Pushing char and strings onto mutable string. In 38:10

--- src/strings.rs
+++ src/strings.rs
@@ -2,11 +2,19 @@
 // String = Growable, heap-allocated data structure - Use when you need to modify or own string data
 pub fn run() {
   let hello = "Hello. Immutable";
-  let hello_growable = String::from("I am growable");
+  let mut hello_growable = String::from("I am growable");
 
   println!("hello: {}", hello);
   
   // Get length
   println!("Length: {}", hello_growable.len());
+  println!("{}", hello_growable);
+
+  // .push for pushing char's
+  hello_growable.push('W');
+  println!("{}", hello_growable);
+
+  // .pushStr from pushing strings
+  hello_growable.push_str("More");
   println!("{}", hello_growable);
 }
